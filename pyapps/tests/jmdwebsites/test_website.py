c81f082e3b4f20740b508afe2f49f7e3ec73d798
jmdwebsites <jmdwebsites@gmail.com>
2016-11-30 11:00:33 +0000

    Tidy up test_instantiation_of_Website

--- pyapps/tests/jmdwebsites/test_website.py
+++ pyapps/tests/jmdwebsites/test_website.py
@@ -7,19 +7,12 @@ import filecmp
 logger = logging.getLogger(__name__)
 
 def test_instantiation_of_Website(setup_test_session, setup_test, tmpdir):
-    site_dir = py.path.local(__file__).dirpath('data/test_website/test_build')
-    with site_dir.as_cwd():
-        logger.info("Change working directory to {}".format(os.getcwd()))
-        # Use the default build directory
-        assert Website().build_dir == site_dir.join('build')
-
-        # Specify the build directory
-        build_dir = os.path.join(os.getcwd(), 'build')
-        assert Website(build_dir).build_dir == build_dir
-
-        # Specify the build directory
-        build_dir = tmpdir.join('build')
-        assert Website(build_dir).build_dir == build_dir
+    # Use the default build directory
+    expected_build_dirname = os.path.join(os.getcwd(), 'build')
+    assert Website().build_dir.strpath == expected_build_dirname
+    # Specify the build directory
+    build_dir = tmpdir.join('build')
+    assert Website(build_dir).build_dir == build_dir
 
 def test_clobber(setup_test_session, setup_test, tmpdir):
     with tmpdir.as_cwd():
