f43bd59d0b9826203b6ae7bc3fee59d31826206e
jmdwebsites <jmdwebsites@gmail.com>
2016-12-10 00:59:09 +0000

    Refactor and rename

--- pyapps/tests/jmdwebsites/test_website.py
+++ pyapps/tests/jmdwebsites/test_website.py
@@ -9,7 +9,6 @@ import six
 def datapath(stem):
     return py.path.local(__file__).dirpath('data', stem)
 
-
 def test_instantiation_of_Website(tmpdir):
     assert Website().build_dir.strpath == os.path.join(os.getcwd(), 'build')
     assert Website(build_dir = tmpdir).build_dir == tmpdir
@@ -51,36 +50,44 @@ def test_build(site_dir, tmpdir):
             assert filecmp.cmp(built.strpath, expected.strpath), \
                 'Page not as expected: {}'.format(built)
 
-
-expected_html_data = {
-    'ext': '.html',
+expected_html_page = {
     'doctype': 'html',
     'charset': 'utf-8'
 }
-@pytest.mark.parametrize("html_file, expected", [
-    (datapath('simple_home_page_and_stylesheet/expected/index.html'), expected_html_data)
+
+@pytest.mark.parametrize("page, expected", [
+    (HtmlPage(datapath('simple_home_page_and_stylesheet/expected/index.html')), expected_html_page)
 ])
-def test_html_page(html_file, expected):
-    print('Check {}'.format(html_file))
-    assert html_file.ext == expected['ext'], \
-        "Incorrect file extension"
-    html_page = HtmlPage(html_file)
-    assert html_page.doctype() == expected['doctype'], \
+def test_html_page(page, expected):
+    assert page.doctype() == expected['doctype'], \
         'Incorrect doctype'
-    assert html_page.charset() == expected['charset'], \
+    assert page.charset() == expected['charset'], \
         'Incorrect charset'
 
+expected_html_file = {
+    'ext': '.html',
+    'page': expected_html_page,
+}
+
+@pytest.mark.parametrize("file, expected", [
+    (datapath('simple_home_page_and_stylesheet/expected/index.html'), expected_html_file)
+])
+def test_html_file(file, expected):
+    print('Check {}'.format(file))
+    assert file.ext == expected['ext'], \
+        "Incorrect file extension"
+    test_html_page(HtmlPage(file), expected['page'])
 
 @pytest.mark.parametrize("input_dir, file_glob, expected", [
-    (datapath('simple_home_page_and_stylesheet/expected'), '*.html', expected_html_data),
-    (py.path.local('site/build'), '*.html', expected_html_data)
+    (datapath('simple_home_page_and_stylesheet/expected'), '*.html', expected_html_file),
+    (py.path.local('site/build'), '*.html', expected_html_file)
 ])
 def test_html_files(input_dir, file_glob, expected):
     for html_file in input_dir.visit(fil = file_glob):
-        test_html_page(html_file, expected)
+        test_html_file(html_file, expected)
 
 @pytest.mark.parametrize("site_dir, expected", [
-    (datapath('simple_home_page_and_stylesheet'), expected_html_data)
+    (datapath('simple_home_page_and_stylesheet'), expected_html_file)
 ])
 def test_html_files_built(site_dir, expected, tmpdir):
     with site_dir.as_cwd():
@@ -88,4 +95,3 @@ def test_html_files_built(site_dir, expected, tmpdir):
         website = Website(build_dir = tmpdir)
         website.build()
         test_html_files(website.build_dir, '*.html', expected)
-        
