c21e273860dacce1ecfdba00119e4617e61af99b
jmdwebsites <jmdwebsites@gmail.com>
2017-02-10 10:19:45 +0000

    In get_content(), use read_text with utf-8 encoding.

--- pyapps/src/jmdwebsites/website.py
+++ pyapps/src/jmdwebsites/website.py
@@ -351,7 +351,7 @@ def get_content(source_dir,
     source_content = {}
     for path in source_dir.visit(fil=fil):
         part_name = path.purebasename.lstrip('_')
-        text = path.read()
+        text = path.read_text(encoding='utf-8')
         if path.ext == '.html':
             html = text
         elif path.ext == '.md':
